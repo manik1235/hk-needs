ea025d1923a57afa38e8615d314ea6f56cf5bf4a
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2018-03-17 11:51:03 -0400

    html, css and javascript all work based on the tutorial. now cloning and making mine!

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,40 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
-var myVariable = 'Bob';
-myVariable;
-
 function multiply(num1,num2) {
 	var result = num1 * num2;
 	return result;
 };
 
-var myHTML = document.querySelector('html');
-myHTML.onclick = function() {
-	alert('Ouch! Stop poking me!');
-};
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/traci-arthur.jpg') {
+		myImage.setAttribute ('src','images/wildsiders.png');
+	} else {
+		myImage.setAttribute ('src','images/traci-arthur.jpg');
+	}
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = `Household Needs, for ${myName}`;
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = `Household Needs, for ${storedName}`;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
+
+var myAvailable = document.getElementById('Andrew.Available.value');
+myAvailable.onclick = function() {
+	myAvailable.textContent = '$20';
+}
