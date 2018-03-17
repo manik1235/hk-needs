4da7de095cb93d00792f6b4f3d36a2258bcc1e55
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2018-03-17 12:51:11 -0400

    first set of changes in structure. added data to the js

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,3 @@
-function multiply(num1,num2) {
-	var result = num1 * num2;
-	return result;
-};
-
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-	var mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/traci-arthur.jpg') {
-		myImage.setAttribute ('src','images/wildsiders.png');
-	} else {
-		myImage.setAttribute ('src','images/traci-arthur.jpg');
-	}
-}
-
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
@@ -34,7 +18,37 @@ myButton.onclick = function() {
 	setUserName();
 }
 
-var myAvailable = document.getElementById('Andrew.Available.value');
+// set the current values for each person
+// set indexes for the array
+var index-traci = 0;
+var index-andrew = 1;
+var index-aydan = 2;
+var index-arthur = 3;
+var index-household = 4;
+
+// Create an array of names
+var names = [];
+names[index-traci] = 'Traci';
+names[index-andrew] = 'Andrew';
+names[index-aydan] = 'Aydan';
+names[index-arthur] = 'Arthur';
+names[index-household] = 'Household';
+
+// This section will be to control the list items for each person.
+// array structure is [person#1, person#2]
+// then the sub arrays are ["Item Name", value, goal, completion-goal-date]
+var items = [];
+items[index-traci] = ['Available', 80, null, null, 'Dresses', 0, 100, null, 'Spring Coat', 0, 60, null];
+items[index-andrew] = ['Available', 80, null, null, 'Haircut', 0, 30, null, 'Dress Shirts', 0, 40, null, 'Ties', 0, 30, null];
+items[index-aydan] = ['Available', 0, null, null, 'Senior Trip 2019', 10, 200, null, 'Movie Ticket', 20, 60, null];
+items[index-arthur] = ['Available', 125, null, null, 'Pants', 0, 40, null];
+items[index-household] = ['Available', 100, null, null, 'Paint, LR', 0, 30, 4/7/2018, 'TV Mount', 0, 30, 3/1/2018];
+
+
+
+
+// Andrew
+var andrewAvailable = document.getElementById('andrew.available.value');
 myAvailable.onclick = function() {
 	myAvailable.textContent = '$20';
 }
